643d4086ed22a84def816f0ff32b11e81863f0f6
Sebastien Sauve <sebastien.sauve@gmail.com>
2010-11-30 21:08:02 -0500

    Some work on the creation of orders/users -- done for the night

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+# Schema version: 20101201004024
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  phone      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+  attr_accessible :name, :email, :phone
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, :length   => { :maximum => 90 }
+              
+  validates :email, :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }                                           
 end
